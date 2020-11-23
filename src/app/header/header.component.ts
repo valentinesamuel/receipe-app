aa87c6821bddaa7290a1c195533b1e5adfbdb245
Valentine <valentinesamuel2580@gmail.com>
2020-11-23 13:01:55 +0100

    Understood the basic layour uo to this point

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './header.component.html',
+  selector: 'app-header',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {}
