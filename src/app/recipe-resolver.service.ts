cf5cc3e51e06b380ba6a7962ebb86f1349f9f9c4
Samuel Valentine <valentinesamuel2580@gmail.com>
2021-04-20 07:10:25 +0100

    (FEAT): Changed the resolver algo to check if there is any locally saved recipe. If not, it will load recipe from the server

--- src/app/recipe-resolver.service.ts
+++ src/app/recipe-resolver.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { RecipeService } from './recipe.service';
 import { Recipe } from './recipes/recipe.modal';
 import { DataStorageService } from './shared/data-storage.service';
 
@@ -8,9 +9,15 @@ import { DataStorageService } from './shared/data-storage.service';
 })
 export class RecipeResolverService implements Resolve<Recipe[]>{
 
-  constructor(private dataStorage:DataStorageService) { }
+  constructor(private dataStorage:DataStorageService, private recipeService:RecipeService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.dataStorage.fetchRecipes();
+    const recipes = this.recipeService.getRecipes();
+
+    if (recipes.length === 0) {
+      return this.dataStorage.fetchRecipes();
+    } else {
+      return recipes;
+    }
   }
 }
